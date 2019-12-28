055c14a6afcc68ce1b14260217d85b09d8d88b2f
AbraCadabstrax <benjamin.blocksom@gmail.com>
2019-12-28 18:55:04 -0500

    Add lightbox modal and anchor images

--- component-lightbox/lightbox.js
+++ component-lightbox/lightbox.js
@@ -1,11 +1,17 @@
+const modalInner = document.querySelector('.modal-inner');
+const modalOuter = document.querySelector('.modal-outer');
+const lightbox = document.querySelector('.lightbox');
+
 function openModal() {
-  document.getElementById("lightbox").style.display = "block";
+  lightbox.classList.add('open');
 }
 
 function closeModal() {
-  document.getElementById("lightbox").style.display = "none";
+  lightbox.classList.remove('open');
+  modalOuter.classList.remove('open');
 }
 
+// Set of functions to move lightbox carousel
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -16,6 +22,7 @@ function plusSlides(n) {
 
 function currentSlide(n) {
   showSlides(slideIndex = n);
+  modalOuter.classList.add('open');
 }
 
 function showSlides(n) {
@@ -29,3 +36,31 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
+
+// Closes modal with click outside window
+modalOuter.addEventListener('click', function(event) {
+  const isOutside = !event.target.closest('.modal-inner');
+  if(isOutside) {
+    closeModal();
+  }
+});
+
+// Set of EventListeners to enable keyboard navigation of carousel
+// Enable closing with 'esc' key
+window.addEventListener('keydown', (event) => {
+  if(event.key === 'Escape')  {
+    closeModal();
+  }
+})
+
+window.addEventListener('keydown', (event) => {
+  if(event.key === 'ArrowLeft')  {
+    plusSlides(-1);
+  }
+})
+
+window.addEventListener('keydown', (event) => {
+  if(event.key === 'ArrowRight')  {
+    plusSlides(1);
+  }
+})
